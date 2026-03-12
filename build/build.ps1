3c62aaaf3512f3876ba6b0da3de4654b5accc5d4
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2026-03-12 18:02:10 +0100

    build: Prepare v4.0.0

--- build/build.ps1
+++ build/build.ps1
@@ -80,13 +80,11 @@ function main {
 
     Set-Location $BuildDir
 
-    Remove-Item 'hashes.txt' -Force
-
     $Hashes = ForEach ($File in (Get-ChildItem -File -Recurse)) {
         Get-FileHash -LiteralPath $File.FullName -Algorithm SHA256 | Select-Object @{N = 'PathRelative'; E = { Resolve-Path -LiteralPath $file.FullName -Relative } }, Algorithm, Hash
     }
 
-    $Hashes | Export-Csv hashes.txt
+    $Hashes | Export-Csv hashes.txt -Force
 
 
     Write-Output 'Create release file'
